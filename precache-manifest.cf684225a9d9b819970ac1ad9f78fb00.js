b58304890a572da663d9f72bc514ba9a0d5fd868
Tanya <fullstackon@gmail.com>
2020-08-11 17:21:58 +0300

    Updates

---
+++ precache-manifest.cf684225a9d9b819970ac1ad9f78fb00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "518a299b5723fc22d817",
+    "url": "/weather-dashboard/static/css/main.08ad0656.chunk.css"
+  },
+  {
+    "revision": "518a299b5723fc22d817",
+    "url": "/weather-dashboard/static/js/main.6ca1da11.chunk.js"
+  },
+  {
+    "revision": "18eb3f1a32e80f41731d",
+    "url": "/weather-dashboard/static/js/runtime~main.4e9c5ee0.js"
+  },
+  {
+    "revision": "a9b3052055d843a7f1d0",
+    "url": "/weather-dashboard/static/js/2.f7d6fe98.chunk.js"
+  },
+  {
+    "revision": "eaa8f37ab471783435838878df847d24",
+    "url": "/weather-dashboard/static/media/weather3.eaa8f37a.jpg"
+  },
+  {
+    "revision": "559fdaa59efa755504d41e880bd6eee7",
+    "url": "/weather-dashboard/index.html"
+  }
+];
